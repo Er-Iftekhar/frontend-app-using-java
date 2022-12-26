4c3a91b179adef9cc5f826c6db2219556b2514cc
syedy636@gmail.com <syedy636@gmail.com>
2022-12-26 14:29:51 +0100

    aspectj maven configuration added.

---
+++ src/main/java/com/app/config/AspectJConfig.java
@@ -0,0 +1,9 @@
+package com.app.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+
+@ComponentScan
+@EnableSpringConfigured
+public class AspectJConfig {
+}
